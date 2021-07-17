6f8d1d3ab9785c627bef44e6e22262bd1cb57883
DMawi17 <asegda17@gmail.com>
2021-07-17 20:18:22 +0200

    to be styled

--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -5,7 +5,7 @@ const Recipe = ({ name, ingredients, steps }) => {
     return (
         <section id={name.toLowerCase().replace(/ /g, "-")}>
             <h2>{name}</h2>
-            <IngredientsList title="Ingredients" list={ingredients} />
+            <IngredientsList title="Ingredien" list={ingredients} />
             <Instructions title="Instructions" steps={steps} />
         </section>
     );
